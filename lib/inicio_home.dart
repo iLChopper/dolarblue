ff1c62cc816280e034a0283d491ebcc1620c8e6c
Rodrigo <rodrigo.rodriguez@live.com.ar>
2022-08-03 13:04:20 -0300

    diseño

--- lib/inicio_home.dart
+++ lib/inicio_home.dart
@@ -1,6 +1,7 @@
 import 'package:dolarblue/api/dolar_api.dart';
 import 'package:dolarblue/model/dolar_model.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Inicio extends StatelessWidget {
   const Inicio({Key? key}) : super(key: key);
@@ -36,38 +37,87 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          precioDolar(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(width: 200, child: campoPeso()),
-              const SizedBox(width: 20),
-              IconButton(
-                icon: Image.asset('android/assets/images/changedolar.png'),
-                iconSize: 50,
-                onPressed: () {
-                  setState(() {
-                    dolarc.text = '';
-                    pesoc.text = '';
-                    if (!isEnabled) {
-                      isEnabled = true;
-                    } else {
-                      isEnabled = false;
-                    }
-                  });
-                },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme(
+            labelStyle: TextStyle(color: Colors.white),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.amberAccent),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.amberAccent),
+            ),
+            disabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white38))),
+        textTheme: const TextTheme(
+            /*  headline1:
+               
+           TextStyle(
+              fontFamily:  
+                fontSize: 36.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87),*/
+            subtitle1: TextStyle(color: Colors.white, fontSize: 25)),
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+                width: 300,
+                height: 300,
+                margin: const EdgeInsets.only(top: 70),
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(53, 55, 88, 1),
+                    shape: BoxShape.circle),
+                child: Center(child: precioDolar())),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(top: 100),
+                padding: const EdgeInsets.only(top: 120),
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(53, 55, 88, 1),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(37.5),
+                      topRight: Radius.circular(37.5),
+                    )),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(width: 60, child: campoPeso()),
+                        const SizedBox(width: 20),
+                        IconButton(
+                          splashColor: Colors.white,
+                          icon: Image.asset(
+                              'android/assets/images/changedolar.png'),
+                          iconSize: 50,
+                          highlightColor: Colors.white,
+                          tooltip: 'Cambio',
+                          onPressed: () {
+                            setState(() {
+                              dolarc.text = '';
+                              pesoc.text = '';
+                              isEnabled = !isEnabled;
+                            });
+                          },
+                        ),
+                        const SizedBox(width: 20),
+                        SizedBox(width: 60, child: campoDolar()),
+                      ],
+                    ),
+                    const SizedBox(height: 50),
+                    boton(),
+                  ],
+                ),
               ),
-              const SizedBox(width: 20),
-              Container(width: 200, child: campoDolar()),
-            ],
-          ),
-          const SizedBox(height: 20),
-          boton(),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -78,11 +128,17 @@ class _HomeState extends State<Home> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             precioDolarBlue = snapshot.data!.blue!.valueBuy.toString();
-            return Text('Precio Dolar Blue: ${snapshot.data!.blue!.valueBuy}');
+            return Text(
+              '${snapshot.data!.blue!.valueBuy}\n Dolar Blue',
+              style: GoogleFonts.pacifico(
+                  textStyle:
+                      const TextStyle(color: Colors.white, fontSize: 40)),
+              textAlign: TextAlign.center,
+            );
           } else if (snapshot.hasError) {
             return Text("${snapshot.error}");
           }
-          return Container();
+          return const CircularProgressIndicator();
         });
   }
 
@@ -93,11 +149,12 @@ class _HomeState extends State<Home> {
             _cambioDivisa();
           });
         },
-        child: const Text('Cambio'));
+        child: const Text('Calcular'));
   }
 
   TextField campoPeso() {
     return TextField(
+        autofocus: true,
         enabled: !isEnabled,
         keyboardType: TextInputType.number,
         controller: pesoc,
